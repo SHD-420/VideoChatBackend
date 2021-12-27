e38895ae6d7c2324cb7bc9722dc79083de4fdaa3
Shivang <32.shivangdubey@gmail.com>
2021-12-28 01:48:11 +0530

    Build: added http

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+const http = require("http");
 const { Server: WebSocketServer } = require("ws");
 const crypto = require("crypto");
 const handleMessages = require("./handlers/message-handler");
 const { incomming } = require("./handlers/message-handler/message-types");
 
-const PORT = 8000;
-const server = new WebSocketServer({
-  port: PORT,
+const PORT = process.env.PORT || 8000;
+
+const httpServer = http.createServer((req, res) => {
+  res.end("Welcome!!");
 });
 
-server.on("connection", (socket) => {
+httpServer.listen(PORT, () => console.log(`Listening on port:${PORT}`));
+
+const wsServer = new WebSocketServer({ server: httpServer });
+
+wsServer.on("connection", (socket) => {
   const socketId = crypto.randomBytes(12).toString("hex");
   socket.id = socketId;
-  handleMessages({ socket, server });
+  handleMessages({ socket, wsServer });
   socket.on("close", () => {
     socket.emit(
       "message",

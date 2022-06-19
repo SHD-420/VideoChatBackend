188615800af558bdf10dde0c4eb1955b34afcf9b
Shivang <32.shivangdubey@gmail.com>
2022-06-19 23:42:05 +0530

    Updated update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@ set -e
 
 sudo rm -rf client
 
-git clone --depth=1 --branch=deploy git@github.com:SHD-420/sws.git client
+git clone --depth=1 --branch=deploy git@github.com:SHD-420/VideoChatFrontend.git client
 
 rm -rf client/.git
